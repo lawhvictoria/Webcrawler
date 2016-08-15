1c55988bd4a8044095d7bf5923725ff27867f707
Victoria Law <victorialaw@wppmbpm01409251-wifi.ptc.ca.kp.org>
2016-08-15 16:09:16 -0700

    Victoria: Added Regex and Robots.txt parser

--- webcrawler.java
+++ webcrawler.java
@@ -1,5 +1,9 @@
 package Webcrawler;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.*;
 
 import org.jsoup.Jsoup;
@@ -7,15 +11,20 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class webcrawler {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String website = "www.kp.org";
+		String website = "www.nasa.gov";
 		ArrayList<String> uriList = new ArrayList<String>();
 		uriList = getLinks(website);
-		printList(uriList);
-//		System.out.println(uriList);
+//		printList(uriList);
+		readRobot(website);
+		
+		
 		
 	}
 	
@@ -46,14 +55,6 @@ public class webcrawler {
 					uriList.add(link.attr("href"));
 				}
 			}
-//			System.out.println();
-//			System.out.println();
-//			System.out.println();
-//			System.out.println();
-//			System.out.println();
-			
-//			String htmlString = doc.toString();
-//			System.out.println(htmlString);
 		}
 		catch(Exception e){
 			e.printStackTrace();
@@ -68,5 +69,38 @@ public class webcrawler {
 			System.out.println(link);
 		}
 	}
-
+	
+	public static void readRobot(String website){
+		try(BufferedReader in = new BufferedReader(new InputStreamReader(new URL("http://" + website + "/robots.txt").openStream()))){
+			String line = null;
+			
+			//Defines a new pattern
+			String pattern = "(\\/)(.*)";
+			
+			//Creates a Pattern object
+			Pattern r = Pattern.compile(pattern);
+			
+			//Looks through Robots.txt until end of file
+			while((line = in.readLine()) != null){
+//				System.out.println(line);
+				
+				//If line contains disallow, Matcher matches each line against the pattern
+				if(line.contains("Disallow:")){
+					Matcher m = r.matcher(line);
+					if(m.find()){
+						System.out.println(m.group(0));
+					}
+					else{
+					System.out.println("No Match");
+					}
+				}
+				
+			}
+			
+		}
+		
+		catch(IOException e){
+			e.printStackTrace();
+		}
+	}
 }
